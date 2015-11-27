f9fe5019f8e7f3e7199da057568843ed0e590a87
Rob Cornacchia <bobbycornacchia@gmail.com>
2015-11-26 23:25:03 -0500

    Added socket io

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ app.get("/", function(req, res){
     res.render("page");
 });
 
+app.use(express.static(__dirname + '/public'));
 
-app.listen(port);
+var io = require('socket.io').listen(app.listen(port));
 console.log("Listening on port " + port);
+
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function(data) {
+        io.sockets.emit('message', data);
+    });
+});
